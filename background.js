725072513db5548ba9a4549253c182196f3013b6
konzertnr9 <miy.msnr@gmail.com>
2021-04-01 16:15:27 +0800

    Initial commit

---
+++ background.js
@@ -0,0 +1,101 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: 'Project',
+    title: 'Open Polyglot project',
+    contexts: ['page'],
+    documentUrlPatterns: ['https://localization.google.com/polyglot/tasks/*'],
+  });
+  chrome.contextMenus.create({
+    id: 'LQE',
+    title: 'Open LQE report',
+    contexts: ['page'],
+    documentUrlPatterns: ['https://localization.google.com/polyglot/tasks/*'],
+  });
+  chrome.contextMenus.create({
+    id: 'devSite',
+    title: 'Open DevSite page',
+    contexts: ['page'],
+    documentUrlPatterns: ['https://localization.google.com/polyglot/tasks/*'],
+  });
+  chrome.contextMenus.create({
+    id: 'help',
+    title: 'Open Help article',
+    contexts: ['selection'],
+    documentUrlPatterns: ['https://localization.google.com/polyglot/tasks/*'],
+  });
+  chrome.contextMenus.create({
+    id: 'QM',
+    title: 'Open QM queries',
+    contexts: ['page'],
+    documentUrlPatterns: ['https://localization.google.com/polyglot/tasks/*'],
+  });
+  chrome.contextMenus.create({
+    id: 'QMSearch',
+    title: 'Search in QM',
+    contexts: ['selection'],
+    documentUrlPatterns: ['https://localization.google.com/polyglot/tasks/*'],
+  });
+  chrome.contextMenus.create({
+    id: 'en',
+    title: 'Open EN page',
+    contexts: ['page'],
+    documentUrlPatterns: ['*://*.google.com/*', '*://*.android.com/*'],
+  });
+});
+chrome.contextMenus.onClicked.addListener((itemData) => {
+  switch (itemData.menuItemId) {
+    case 'Project':
+      chrome.tabs.query({active:true, currentWindow:true}, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, {message: 'openProjectPage'},
+            (res) => {
+          console.log(res);
+        });
+      });
+      break;
+    case 'LQE':
+      chrome.tabs.query({active:true, currentWindow:true}, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, {message: 'openLqePage'}, 
+            (res) => {
+          console.log(res);
+        });
+      });
+      break;
+    case 'devSite':
+      chrome.tabs.query({active:true, currentWindow:true}, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, {message: 'openDevSitePage'}, 
+            (res) => {
+          console.log(res);
+        });
+      });
+      break;
+    case 'help':
+      chrome.tabs.query({active:true, currentWindow:true}, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, {message: 'openHelpArticlePage'}, 
+            (res) => {
+          console.log(res);
+        });
+      });
+      break;
+    case 'QM':
+      chrome.tabs.query({active:true, currentWindow:true}, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, {message: 'openQMPage'}, (res) => {
+          console.log(res);
+        });
+      });
+      break;
+    case 'QMSearch':
+      chrome.tabs.query({active:true, currentWindow:true}, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, {message: 'searchInQm'}, (res) => {
+          console.log(res);
+        });
+      });
+      break;
+    case 'en':
+      chrome.tabs.query({active:true, currentWindow:true}, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, {message: 'openEnPage'}, (res) => {
+          console.log(res);
+        });
+      });
+      break;
+  }
+});
---
+++ contentScript.js
@@ -0,0 +1,231 @@
+function dEBCN(elm) {
+  return document.getElementsByClassName(elm);
+}
+
+function devSitePage() {
+  const hosts = {
+    'Cloud Platform': 'cloud.google.com',
+    'Cloud Platform Priority 1': 'cloud.google.com',
+    'Cloud Platform Priority 2': 'cloud.google.com',
+    'Cloud Platform Priority 3': 'cloud.google.com',
+    'Cloud Console': 'cloud.google.com',
+    'Cloud Marketing & Sales': 'cloud.google.com',
+    'Cloud Technical Marketing': 'cloud.google.com',
+    'Firebase': 'firebase.google.com',
+    'Apigee': 'docs.apigee.com',
+    'Search Console': 'developers.google.com',
+    'Android': ['developer.android.com', 'source.android.com'],
+  };
+  const title_arr = dEBCN('ZY4IA')[0].getAttribute('title').split('_');
+  const locale = title_arr.shift();
+  const product = title_arr.shift();
+  const project_id = title_arr.shift();
+  const raw_path = title_arr.join('_');
+  let path_arr = raw_path.split('/');
+  path_arr[0] = path_arr[0].replace('Translatable HTML attributes for ', '');
+  path_arr[0] = path_arr[0].replace('Meta tags for ', '');
+  const file_name = path_arr[path_arr.length - 1];
+  if (!file_name.match('.html') && !file_name.match('.arb')) {
+    let prompt = 
+        'This does not look like a DevSite project. ' + 
+        'Are you sure to proceed?';
+    if (!window.confirm(prompt)) {
+      return false;
+    }
+  }
+  if (file_name == 'index.html' || 
+      file_name.match('.arb') || 
+      file_name.match('_index.yaml')) {
+    path_arr[path_arr.length - 1] = '';
+  } else {
+    path_arr[path_arr.length - 1] = file_name.replace('.html', '');
+  }
+  let url;
+  if (Array.isArray(hosts[product])) { // Multiple possible sites
+    let url_arr = [];
+    for (const host of hosts[product]) {
+      // Check project description for target site
+      let inst = dEBCN('r99c0e')[0].innerHTML;
+      url = 'https://' + host + '/' + path_arr.join('/') + '?hl=' + locale;
+      if (inst && inst.match(host)) {
+        return window.open(url) ? url : false;
+      } else {
+        url_arr.push(url);
+      }
+    }
+    let ret_val = [];
+    for (const url of url_arr) {
+      if (!window.open(url)) {
+        return false;
+      }
+      ret_val.push(url);
+    }
+    return ret_val.join(',');
+  } else {
+    url = 
+        'https://' + hosts[product] + '/' + path_arr.join('/') + 
+        '?hl=' + locale;
+    return window.open(url) ? url : false;
+  }
+}
+
+function enPage() {
+  const loc = window.location;
+  const cur_qs = loc.search.slice(1);
+  let new_qs = 'hl=en';
+  if (cur_qs) {
+    let qs_arr = cur_qs.split('&');
+    let lang_flg = false;
+    let k, v;
+    for (let i = 0; i < qs_arr.length; i++) {
+      [k, v] = qs_arr[i].split('=');
+      if (k == 'hl') {
+        qs_arr[i] = 'hl=en';
+        lang_flg = true;
+      }
+    }
+    if (!lang_flg) {
+      qs_arr.push('hl=en');
+    }
+    new_qs = qs_arr.join('&');
+  }
+  const url = 
+      loc.protocol + '//' + loc.host + loc.pathname + 
+      '?' + new_qs + (loc.hash ? loc.hash : '');
+  return window.open(url) ? url : false;
+}
+
+function projectPage() {
+  const project_id = dEBCN('ZY4IA')[0].getAttribute('title').split('_')[2];
+  const url = 
+      'https://localization.google.com/polyglot?project_id=' + project_id;
+  return window.open(url) ? url : false;
+}
+
+function helpArticlePage() {
+  const sel = window.getSelection().toString();
+  let locale, product, project_id, raw_path;
+  [locale, product, project_id, raw_path] = 
+      dEBCN('ZY4IA')[0].getAttribute('title').split('_');
+  let url;
+  if (~sel.indexOf('/')) {
+    if (sel.indexOf('answer') == -1) {
+      let prompt = 
+          'This does not look like a link to a Help article. ' + 
+          'Are you sure to proceed?';
+      if (!window.confirm(prompt)) {
+        return false;
+      }
+    }
+    let art = sel;
+    if (sel.indexOf('/') > 0) {
+      art = '/' + art;
+    }
+    url = 'https://support.google.com' + art + '?hl=' + locale;
+    return window.open(url) ? url : false;
+  }
+  const dirs = {
+    'Allo': 'allo',
+    'Ad Grants': 'grants',
+    'Android': 'android',
+    'Calendar':  'calendar',
+    'Chrome': 'chrome',
+    'Cloud Platform': 'googlecloud',
+    'Cloud Platform Priority 1': 'googlecloud',
+    'Cloud Platform Priority 2': 'googlecloud',
+    'Cloud Platform Priority 3': 'googlecloud',
+    'Cloud Console': 'googlecloud',
+    'Cloud Marketing & Sales': 'googlecloud',
+    'Cloud Technical Marketing': 'googlecloud',
+    'Docs': 'docs',
+    'Duo': 'duo',
+    'Family Link': 'families',
+    'GAIA': 'accounts',
+    'Gmail': 'mail',
+    'Google Assistant': 'assistant',
+    'Google Domains': 'domains',
+    'Google Fi': 'fi',
+    'Google My Business (GMB)': 'business',
+    'Google Play': 'play',
+    'Google One': 'googleone',
+    'Google Store': 'store',
+    'Hangouts': 'hangouts',
+    'Hangouts Chat': 'chat',
+    'Hangouts Meet': 'meet',
+    'Maps': 'maps',
+    'Payments': ['pay', 'paymentscenter'],
+    'Photos': 'photos',
+    'Product Search': 'merchants',
+    'Search (GWS)': 'websearch',
+    'Search Console': 'webmasters',
+    'Shopping Merchants and Brands': 'merchants',
+    'YouTube': 'youtube',
+    'YouTube Creator': 'youtube',
+    'YouTube MT Pilot': 'youtube',
+  };
+  if (Array.isArray(dirs[product])) { // Multiple possible directories
+    let ret_val = [];
+    for (const dir of dirs[product]) {
+      url = 
+          'https://support.google.com/' + dir + '/answer/' + sel + 
+          '?hl=' + locale;
+      if (!window.open(url)) {
+        return false;
+      }
+      ret_val.push(url);
+    }
+    return ret_val.join(',');
+  } else {
+    url = 
+        'https://support.google.com/' + dirs[product] + '/answer/' + sel + 
+        '?hl=' + locale;
+    return window.open(url) ? url : false;
+  }
+}
+
+function lqePage() {
+  const url_arr = window.location.href.split('/');
+  const url = 
+      'https://localization.google.com/lqe/arbitration/' + 
+      url_arr[url_arr.length - 1];
+  return window.open(url) ? url : false;
+}
+
+function qmPage() {
+  const project_id = dEBCN('ZY4IA')[0].getAttribute('title').split('_')[2];
+  const url = 'https://gloc-qm.appspot.com/?show_all=yes&search=' + project_id;
+  return window.open(url) ? url : false;
+}
+
+function qmSearch() {
+  const url = 
+      'https://gloc-qm.appspot.com/?show_all=yes&search=' + 
+      encodeURIComponent(window.getSelection().toString());
+  return window.open(url) ? url : false;
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  switch (request.message) {
+    case 'openProjectPage':
+      sendResponse(projectPage());
+      break;
+    case 'openEnPage':
+      sendResponse(enPage());
+      break;
+    case 'openDevSitePage':
+      sendResponse(devSitePage());
+      break;
+    case 'openHelpArticlePage':
+      sendResponse(helpArticlePage());
+      break;
+    case 'openLqePage':
+      sendResponse(lqePage());
+      break;
+    case 'openQMPage':
+      sendResponse(qmPage());
+      break;
+    case 'searchInQm':
+      sendResponse(qmSearch());
+      break;
+  }
+});
