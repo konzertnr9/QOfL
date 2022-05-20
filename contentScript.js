cad3378c92116b2e4f9095674fe585eb524703d8
konzertnr9 <miy.msnr@gmail.com>
2022-05-20 16:46:58 +0800

    Added support for Firebase pages in DevPlat projects (DevSite)

--- contentScript.js
+++ contentScript.js
@@ -60,7 +60,7 @@ function devSitePage() {
     'Cloud Marketing & Sales': 'cloud.google.com',
     'Cloud Technical Marketing': 'cloud.google.com',
     'Cloud-MT Pilot': 'cloud.google.com',
-    'DevPlat': 'developers.google.com',
+    'DevPlat': ['developers.google.com', 'firebase.google.com'],
     'Firebase': 'firebase.google.com',
     'Google Assistant': 'developers.google.com',
     'Maps': 'developers.google.com',
